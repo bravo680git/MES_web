d19262c29699bbc95360f5dc5be1ade9a3b691b4
bravo680git <iambravo680@gmail.com>
2023-03-28 09:43:49 +0700

    create checkbox component

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,36 @@
+import { useRef } from "react"
+
+function Checkbox({ id, label, value, setValue }) {
+    const cbRef = useRef()
+
+    const handleClick = (e) => {
+        cbRef.current.click()
+    }
+
+    return (
+        <div
+            data-component="Checkbox"
+            className="group flex h-10 cursor-pointer items-center border-b-2 border-primary-3 hover:border-primary-2"
+            onClick={handleClick}
+        >
+            <input
+                ref={cbRef}
+                type="checkbox"
+                id={id}
+                className="h-5 w-5"
+                checked={value}
+                onChange={(e) => setValue(e.target.checked, id)}
+                onClick={(e) => e.stopPropagation()}
+            />
+            <label
+                htmlFor={id}
+                className="text-16-b ml-4 cursor-pointer group-hover:text-primary-2"
+                onClick={(e) => e.stopPropagation()}
+            >
+                {label}
+            </label>
+        </div>
+    )
+}
+
+export default Checkbox
