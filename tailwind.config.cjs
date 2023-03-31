39f813c56efc102ae0a825271f21e42ee03fca88
bravo680git <iambravo680@gmail.com>
2023-03-31 16:12:16 +0700

    responsive sidebar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,6 +30,7 @@ module.exports = {
             hoverBg: "rgba(0,0,0,0.08)",
         },
         screens: {
+            xxl: { max: "1600px" },
             xl: { max: "1279px" },
 
             lg: { max: "1023px" },
