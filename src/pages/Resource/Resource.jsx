b7ae407a4f88d3109bb665ef7dec135f74f6aae0
bravo680git <iambravo680@gmail.com>
2023-05-04 14:31:14 +0700

    display production schedule

--- src/pages/Resource/Resource.jsx
+++ src/pages/Resource/Resource.jsx
@@ -5,19 +5,15 @@ import ResourceItem from "@/components/ResourceItem"
 import PoperMenu from "@/components/PoperMenu"
 import { usePoperMenu, useCallApi } from "@/hooks"
 import { resourceApi } from "@/services/api"
-import { getCreateWorkerMenuNav, getCreateEquipmentMenuNav, getCreateMaterialMenuNav } from "@/utils/menuNavigation"
-import { EQUIPMENT_TABLE_COLUMNS, MATERIAL_TABLE_COLUMNS, WORKER_TABLE_COLUMNS } from "@/utils/tableColumns"
+import { getCreateEquipmentMenuNav, getCreateMaterialMenuNav } from "@/utils/menuNavigation"
+import { EQUIPMENT_TABLE_COLUMNS, MATERIAL_TABLE_COLUMNS } from "@/utils/tableColumns"
 import { paths } from "@/config"
 
 const handler = {
-    labels: ["Nhân viên", "Thiết bị", "Vật tư"],
-    headers: [WORKER_TABLE_COLUMNS, EQUIPMENT_TABLE_COLUMNS, MATERIAL_TABLE_COLUMNS],
-    getMenuNavs: [
-        (list) => getCreateWorkerMenuNav(list),
-        (list) => getCreateEquipmentMenuNav(list),
-        (list) => getCreateMaterialMenuNav(list),
-    ],
-    navigateLinks: [paths.resource + "/worker", paths.resource + "/equipment", paths.resource + "/material"],
+    labels: ["Thiết bị", "Vật tư"],
+    headers: [EQUIPMENT_TABLE_COLUMNS, MATERIAL_TABLE_COLUMNS],
+    getMenuNavs: [(list) => getCreateEquipmentMenuNav(list), (list) => getCreateMaterialMenuNav(list)],
+    navigateLinks: [paths.resource + "/equipment", paths.resource + "/material"],
 }
 
 function Resource() {
@@ -36,20 +32,13 @@ function Resource() {
     const handleLabelClick = (index) => navigate(handler.navigateLinks[index])
 
     useEffect(() => {
-        callApi(
-            [
-                resourceApi.worker.getWorkers(),
-                resourceApi.equipment.getEquipments(),
-                resourceApi.material.getMaterials(),
-            ],
-            (res) => setResData(res),
-        )
+        callApi([resourceApi.equipment.getEquipments(), resourceApi.material.getMaterials()], (res) => setResData(res))
     }, [callApi])
 
     return (
         <div data-component="Resource" className="container flex h-full flex-wrap">
             {resData.map((data, index) => (
-                <div className="h-full w-1/3 pr-5 last:pr-0 xl:mb-4 xl:h-full xl:w-full xl:pr-0" key={index}>
+                <div className="h-full w-1/2 pr-5 last:pr-0 xl:mb-4 xl:h-full xl:w-full xl:pr-0" key={index}>
                     <ResourceItem
                         label={handler.labels[index]}
                         headers={handler.headers[index]}
---
+++ src/utils/tableColumns/productionSchedule.js
@@ -0,0 +1,30 @@
+export const PRODUCTION_SCHEDULE_TABLE_COLUMNS = [
+    {
+        Header: "ID lệnh sản xuất",
+        accessor: "workOrderId",
+    },
+    {
+        Header: "Mô tả",
+        accessor: "description",
+    },
+    {
+        Header: "ID sản phẩm",
+        accessor: "materialDefinition",
+    },
+    {
+        Header: "Thiết bị",
+        accessor: "equipment",
+    },
+    {
+        Header: "Ngày bắt đầu",
+        accessor: "scheduledStartDate",
+    },
+    {
+        Header: "Ngày hoàn thành",
+        accessor: "scheduledEndDate",
+    },
+    {
+        Header: "Số lượng",
+        accessor: "quantity",
+    },
+]
