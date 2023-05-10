3773bca1b53c54d3a9d5426e8c2196fe4e56b1cb
bravo680git <iambravo680@gmail.com>
2023-05-10 11:36:47 +0700

    add pageSize in resource api

--- src/services/api/core/resource.js
+++ src/services/api/core/resource.js
@@ -15,7 +15,7 @@ const resourceApi = {
         createEquipmentClass: async (data) => await axiosClient.post("/equipmentClasses", data),
         updateEquipmentClass: async (data, classId) => await axiosClient.put(`/equipmentClasses/${classId}`, data),
 
-        getEquipments: async () => await axiosClient.get("/equipments"),
+        getEquipments: async () => await axiosClient.get("/equipments?PageIndex=1&PageSize=1000"),
         createEquipment: async (data) => await axiosClient.post("/equipments", data),
         updateEquipment: async (data, workerId) => await axiosClient.put(`/equipments/${workerId}`, data),
     },
@@ -24,7 +24,7 @@ const resourceApi = {
         createMaterialClass: async (data) => await axiosClient.post("/materialClasses", data),
         updateMaterialClass: async (data, classId) => await axiosClient.put(`/materialClasses/${classId}`, data),
 
-        getMaterials: async () => await axiosClient.get("/materialDefinitions"),
+        getMaterials: async () => await axiosClient.get("/materialDefinitions?PageIndex=1&PageSize=1000"),
         createMaterial: async (data) => await axiosClient.post("/materialDefinitions", data),
         updateMaterial: async (data, workerId) => await axiosClient.put(`/materialDefinitions/${workerId}`, data),
     },
